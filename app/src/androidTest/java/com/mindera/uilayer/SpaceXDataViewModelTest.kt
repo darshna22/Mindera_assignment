f695528367b4c0c8c5dc5a608c883b8ad9a36ce0
karishma <karishmakumari724789@gmail.com>
2022-07-19 00:24:44 +0530

    build.gradle changes

---
+++ app/src/androidTest/java/com/mindera/uilayer/SpaceXDataViewModelTest.kt
@@ -0,0 +1,10 @@
+package com.mindera.uilayer
+
+import org.junit.Before
+
+class SpaceXDataViewModelTest{
+    lateinit var spaceXDataViewModelTest:SpaceXDataViewModelTest
+    @Before
+    fun init(){
+    }
+}
